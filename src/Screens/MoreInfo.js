be116934fd9b6934cd3722df0ec97b845291fe79
Sikariuss <sikariussumbra@gmail.com>
2020-10-12 17:45:44 +0200

    Update screen MoreInfo

--- src/Screens/MoreInfo.js
+++ src/Screens/MoreInfo.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity, Image } from 'react-native';
 
 import DropDownItem from 'react-native-drop-down-item';
 
-import essenceData from '../helpers/EssenceArbre'
-
-const IC_ARR_DOWN = require('../Ressources/Images/ic_arr_down.png');
-const IC_ARR_UP = require('../Ressources/Images/ic_arr_up.png');
+import essenceData from '../helpers/EssenceArbre';
+import {getEssenceFromDataWithSearchedText} from '../helpers/EssenceArbre';
 
 class MoreInfo extends React.Component {
     constructor(props) {
         super(props)
+
+        this.searchedText = ""
 
         this.state = {
             contents: essenceData
         }
     }
 
+    _searchTextInputChanged(text) {
+      this.searchedText = text
+
+      if (this.searchedText.length > 0) {
+        this.setState({
+          contents: getEssenceFromDataWithSearchedText(this.searchedText)
+        })
+      }
+      else {
+        this.setState({
+          contents: essenceData
+        })
+      }
+    }
   render() {
     return (
         <View style={styles.container}>
+          <TextInput
+            style={styles.search}
+            placeholder='Recherchez une essence'
+            placeholderTextColor='black'
+            onChangeText={(text) => this._searchTextInputChanged(text)}/>
+          <View style={{alignItems: 'center'}}>
+            <View
+              style={{
+                backgroundColor: 'black',
+                width: '50%',
+                height: 2,
+                marginBottom: 10,
+              }}
+            ></View>
+          </View>
         <ScrollView style={{ alignSelf: 'stretch' }}>
           {
             this.state.contents
@@ -29,53 +58,91 @@ class MoreInfo extends React.Component {
                     key={i}
                     style={styles.dropDownItem}
                     contentVisible={false}
-                    invisibleImage={IC_ARR_DOWN}
-                    visibleImage={IC_ARR_UP}
+                    invisibleImage={null}
+                    visibleImage={null}
+                    backgroundColor='#ccc'
                     header={
-                      <View>
-                        <Text style={styles.title}>{param.title}</Text>
+                      <View style={styles.header}>
+                        <Text style={styles.headerText}>{param.title}</Text>
                       </View>
                     }
                   >
-                    <Text style={[styles.txt,{fontSize: 20,}]}>{param.body}</Text>
+                    <Text style={styles.bodyText}>{param.body}</Text>
                   </DropDownItem>
                 );
               })
               : null
           }
           <View style={{ height: 96 }}/>
+          <TouchableOpacity onPress = {() => this.props.navigation.navigate("Accueil")} style={styles.button}>
+            <Image
+              source = {require('../Ressources/Images/goBack.png')}
+              style = {styles.goBack_image}
+            />
+          </TouchableOpacity>
         </ScrollView>
       </View>
     )}
 }
 
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  title: {
-    fontSize: 22,
-    color: 'black',
-    fontWeight: 'bold'
+  search: {
+    margin: 20,
+    borderColor: 'black',
+    borderWidth: StyleSheet.hairlineWidth,
+    height: 40,
+    borderRadius: 10,
+    paddingHorizontal: 15,
   },
   header: {
     width: '100%',
-    paddingVertical: 8,
-    paddingHorizontal: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
     flexWrap: 'wrap',
     flexDirection: 'row',
     alignItems: 'center',
+    backgroundColor: '#aaa',
+    borderColor: '#999',
+    borderTopWidth: 2,
+    borderBottomWidth: 2
   },
-  headerTxt: {
-    fontSize: 12,
-    color: 'rgb(74,74,74)',
+  headerText: {
+    fontSize: 22,
+    color: 'black',
+    fontWeight: 'bold',
     marginRight: 60,
     flexWrap: 'wrap',
   },
-  txt: {
-    fontSize: 14,
+  bodyText: {
+    fontSize: 18,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    marginTop: 15,
+    marginRight: 5,
+    marginBottom: 15,
+
+    padding: 5,
+
+    height:54,
+    width:54,
+
+    borderRadius:27,
+
+    backgroundColor:'#eee',
+
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  goBack_image: {
+    width: 32,
+    height: 32
   },
 });
 
--- src/helpers/EssenceArbre.js
+++ src/helpers/EssenceArbre.js
@@ -1,14 +1,30 @@
 export default essenceData = [
     {
         title: 'Chêne',
-        body: "Plus d'informations sur le chêne."
+        body: "Plus d'informations sur le chêne.",
     },
     {
         title: "Tilleul",
-        body: "Plus d'information sur le tilleul."
+        body: "Plus d'information sur le tilleul.",
     },
     {
         title: 'Douglas Géant',
-        Body: "Plus d'informations sur le douglas géant."
-    }
+        body: "Plus d'informations sur le douglas géant.",
+    },
+    {
+        title: 'Chêne2',
+        body: "Plus d'informations sur le chêne 2.",
+    },
 ]
+
+export function getEssenceFromDataWithSearchedText(text) {
+  var essenceSearch = []
+  for (var i=0; essenceData[i]; i++) {
+    var essence = essenceData[i]
+    if(essence.title.includes(text)) {
+      essenceSearch.push(essence)
+    }
+  }
+  console.log(essenceSearch)
+  return essenceSearch
+}
