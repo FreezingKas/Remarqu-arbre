e742c767e84d2ef72a8112e88fe65536439d017d
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2020-10-27 17:20:18 +0100

    Firestore foncitonnel

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import { StyleSheet, Text, View, Image, YellowBox, ActivityIndicator } from 'react-native';
-import MapView, {Marker, Callout} from 'react-native-maps';
+import { StyleSheet, View, YellowBox, ActivityIndicator } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 import mapStyle from '../helpers/styleMap';
 
 // firebase SDK module
@@ -10,13 +10,21 @@ import 'firebase/firestore';
 import firebaseConfig from '../helpers/firebase'
 
 
-// faut pas faire ca 
-YellowBox.ignoreWarnings(['Setting a timer']);
+
+
+// on check si l'instance de firebase a déja été crée si non on la crée
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
+
+// on instancie firestore dans une variable c'est obligatoire (jsp pas pourquoi)
 const db = firebase.firestore();
 
+// faut pas faire ca normalement
+YellowBox.ignoreWarnings(['Setting a timer']);
+
+
+// array contenant toutes les données de chaque arbre
 let data = [];
 
 export default class MyMap extends React.Component {
@@ -25,26 +33,28 @@ export default class MyMap extends React.Component {
         this.getData()    
     }
 
+    // état si les donnés sont chargés 
     state = {
         isLoaded: false
     }
 
     async getData() {
+        // récupération des identifiants d'accès aux données de la collection ARBRE dans firestore
         const snapshot = db.collection("ARBRE");
+        // récupération des données
         const arbre = await snapshot.get();
+        // classique boucle pour ajouter chaque arbre à l'array 
         arbre.forEach(doc => {
-            //console.log(doc.id, '=>', doc.data());
             data.push(doc.data());
           });
-        console.log(data)
-        this.setState({isLoaded: true})
-
         
+        // changement d'état afin de mettre fin au chargement
+        this.setState({isLoaded: true})     
     }
 
     
-
     render() {
+        // screen quand les données des arbres ne sont pas encore récupérés
         if (!this.state.isLoaded) {
             return (
                 <View style={styles.mapStyle}>
@@ -52,6 +62,7 @@ export default class MyMap extends React.Component {
                 </View>
                 
             )
+        // écran classique avec la map
         } else if(this.state.isLoaded) {
             return(
                 <View style={styles.mapStyle}>
@@ -66,89 +77,22 @@ export default class MyMap extends React.Component {
                             longitudeDelta: 0.0421,
                         }}
                         customMapStyle={mapStyle}>
-                            {/* <Marker key={0}
-                                    coordinate={{
-                                        latitude: 49.128333,
-                                        longitude: 3.466944,
-                                        latitudeDelta: 0.0922,
-                                        longitudeDelta: 0.0421,
-                                    }}
-                                    title={"Chêne - Beuvardes"}
-                                    description={"Description de L'Arbre 0"}
-                                    image={require('../Ressources/Images/ArbreIcon.png')}/>
-                            <Marker key={1}
-                                    coordinate={{
-                                        latitude: 46.124722,
-                                        longitude: 3.416389,
-                                        latitudeDelta: 0.0922,
-                                        longitudeDelta: 0.0421,
-                                    }}
-                                    title={"Arbres du parc Napoléon III - Vichy"}
-                                    description={"Description de L'Arbre 1"}
-                                    image={require('../Ressources/Images/ArbreIcon.png')}/>
-                            <Marker key={2}
-                                    coordinate={{
-                                        latitude: 43.686389,
-                                        longitude: 7.22,
-                                        latitudeDelta: 0.0922,
-                                        longitudeDelta: 0.0421,
-                                    }}
-                                    title={"Caroubiers - Nice"}
-                                    description={"Description de L'Arbre 2"}
-                                    image={require('../Ressources/Images/ArbreIcon.png')}/>
-                            <Marker key={3}
-                                    coordinate={{
-                                        latitude: 44.546944,
-                                        longitude: 0.434444,
-                                        latitudeDelta: 0.0922,
-                                        longitudeDelta: 0.0421,
-                                    }}
-                                    title={"Chêne - Tombeboeuf"}
-                                    description={"Description de L'Arbre 3"}
-                                    image={require('../Ressources/Images/ArbreIcon.png')}/>
-                          <Marker key={4}
-                                  coordinate={{
-                                      latitude: 48.202778,
-                                      longitude: 7.278889,
-                                      latitudeDelta: 0.0922,
-                                      longitudeDelta: 0.0421,
-                                  }}
-                                  title={"Douglas géant - Ribeauvillé"}
-                                  description={"Description de L'Arbre 4"}
-                                  image={require('../Ressources/Images/ArbreIcon.png')}/>
-                        <Marker key={5}
-                                coordinate={{
-                                    latitude: 48.288786,
-                                    longitude: 6.950237,
-                                    latitudeDelta: 0.0922,
-                                    longitudeDelta: 0.0421,
-                                }}
-                                title={"Tilleul - St Dié-des-Vosges"}
-                                description={"Description de L'Arbre 5"}
-                                image={require('../Ressources/Images/ArbreIcon.png')}/> */}
                                 {
                                     data.map((marker) => {
-                                        <Marker key={marker.id}
+                                        return(
+                                            <Marker
                                                 coordinate={{
                                                     latitude: marker.latitude,
                                                     longitude: marker.longitude,
                                                     latitudeDelta: 0.0922,
-                                                    longitudeDelta: 0.0421,
+                                                    longitudeDelta: 0.0421
                                                 }}
-                                                title={marker.name +  " - " + marker.ville}
-                                        />
-    
+                                                title={marker.name}
+                                                key={marker.id}
+                                            />
+                                        )
                                     })
                                 }
-                                {/* <Marker key={0}
-                                        coordinate={{
-                                            latitude: data[0].latitude,
-                                            longitude: data[0].longitude,
-                                            latitudeDelta: 0.0922,
-                                            longitudeDelta: 0.0421,
-                                        }}
-                                        title={data[0].name +  " - " + data[0].ville}
-                                        /> */}
                     </MapView>
                 </View>
             )
